2e748809cd05efa1655863f01d3834b440f0f650
Enrique Bazua <enriquegbazua@gmail.com>
2021-11-17 23:42:23 -0700

    patterns

--- 01-patrones-creacion/abstract-factory.php
+++ 01-patrones-creacion/abstract-factory.php
@@ -185,7 +185,7 @@ class TwigRenderer implements TemplateRenderer
 {
     public function render(string $templateString, array $arguments = []): string
     {
-        return \Twig::render($templateString, $arguments);
+        /* return \Twig::render($templateString, $arguments); */
     }
 }
 
